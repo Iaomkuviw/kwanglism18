03c03e653e6ecb8a8e02666c8d3e554a15dc8f27
Admin <Admin@SCM-20180201CRX>
2018-04-19 23:49:29 +0700

    กุส่งแล้ววววว

--- src/logoshop/checkYod.java
+++ src/logoshop/checkYod.java
@@ -2,6 +2,6 @@ package logoshop;
 abstract  class checkYod {
      public double calculatecheck(){//เอาตัวเลขที่ขายได้มาเก็บไว้เป้นอาเรย์ลิสต์//
         double commission = 0.5;
-        System.out.println("Commission : " + price);
+        System.out.println("Commission : " + price-(price));
      }
 }
